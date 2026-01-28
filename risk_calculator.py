7b1530f9706834b6ca100e72fed9a16ad4757fb4
darshangohel25 <15120524031@atmiyauni.edu.in>
2026-01-28 08:42:49 +0530

    Add files via upload

---
+++ risk_calculator.py
@@ -0,0 +1,7 @@
+def calculate_risk(rainfall, groundwater):
+    if rainfall < 400 and groundwater < 50:
+        return "High Risk"
+    elif rainfall < 700:
+        return "Medium Risk"
+    else:
+        return "Low Risk"
